ca31ac73daf1131735de11991b551bacb19d3cb5
Rich Daley <rich@richd.me>
2013-03-15 23:19:34 +0000

    Actually import real station data into the DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'yaml'
+
+dir = File.dirname(__FILE__)
+stations = YAML.load(File.open("#{dir}/stations.yml"))
+stations.each do |hash|
+  Station.create(hash)
+end
